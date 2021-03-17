1e70b4dc0de8491635d6ebe542826d2c8d20cfb9
Matt Melendez <matthewemelendez@gmail.com>
2021-03-17 15:13:58 -0700

    Update 03,09 - Complete 11,12

--- problems/12-exactly.js
+++ problems/12-exactly.js
@@ -26,9 +26,29 @@ let result4 = exactly(['follow', 'the', 'yellow', 'brick', 'road'], 0, function 
 console.log(result4); // true
 
 *******************************************************************************/
-
-let exactly = function() {
-
+/*Write a function `exactly` that accepts an array, a number, and a callback as
+arguments. The function should return a boolean indicating whether or not there are
+exactly `number` elements of the array that return true when passed into the callback. */
+//Understand
+// Accepting array, number, & callback
+// Check to see if exactly number elements that pass true
+// If so return true, if not return false
+// Plan
+// Add parameters
+// Create truetracker count
+// Iterate through array
+    //If callback applied to currEl is true
+        // increment truetracker
+// If truetracker === number, return true
+let exactly = function(array, number, callback) {
+    let count = 0;
+    for (let i = 0; i < array.length;i++) {
+        let currEl = array[i];
+        if (callback(currEl)) {
+            count++;
+        }
+    }
+   return (count === number)
 };
 
 
