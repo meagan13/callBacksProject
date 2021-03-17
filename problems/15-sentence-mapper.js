52741fa7fd0e9436adaf1052f0402737f70ff53b
meagan <meagan.h.smith@gmail.com>
2021-03-17 18:01:02 -0500

    Complete problem 15

--- problems/15-sentence-mapper.js
+++ problems/15-sentence-mapper.js
@@ -23,10 +23,35 @@ let removeVowels = function(word) {
 
 let result2 = sentenceMapper("this is pretty cool right", removeVowels);
 console.log(result2); // 'ths s prtty cl rght'
+
+Write a function `sentenceMapper` that accepts a sentence and a callback as arguments.
+The function should return a new sentence where every word of the original sentence
+becomes the result of passing the word to the callback.
+
 *******************************************************************************/
+//Understand
+//  Accepts a sentence and a callback
+//  Use callback on every word
+//  Return new sentence
+//Plan
+//  Split up sentence
+//  forEach loop
+//      Apply callback to each word
+//      Push new word into new sentence
+//  Return new sentence.join()
 
-let sentenceMapper = function() {
+let sentenceMapper = function(sentence, callback) {
+    let words = sentence.split(' ');
 
+    /*for(let i = 0; i < words.length; i++) {
+        let curWord = words[i];
+
+        words[i] = callback(curWord);
+    }*/
+    words.forEach(callback(word) {
+        word = callback(word);
+    });
+    return words.join(' ');
 };
 
 
