e1dfa8d32e7a859a1fb1d594574a5b6e99b5d74f
Matt Melendez <matthewemelendez@gmail.com>
2021-03-17 13:18:20 -0700

    Complete 03, 04

--- problems/04-my-filter.js
+++ problems/04-my-filter.js
@@ -18,9 +18,31 @@ let result2 = myFilter(['choose', 'big', 'words', 'only'], function (s) {
 });
 console.log(result2);      // ['choose', 'words', 'only']
 *******************************************************************************/
+/* Write a function `myFilter` that accepts an array and a callback as arguments.
+The function should call the callback on each element of the array, passing in
+the element. The function should return a new array containing
+the elements that result in true when passed to the callback. */
+//Understand
+    //Take in an array and a callback
+    // Apply callback to each element
+    // Return new array with all elements that pass callback filter
+//Plan
+    // Array and Callback as parameters *
+    // Create empty answer array *
+    // Will loop through array argument *
+        //Apply callback to each element to deteremine T/F
+        // If true, push into answer array
+    // Return answer array
 
-let myFilter = function() {
-
+let myFilter = function(array,callback) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        let currEl = array[i];
+        if (callback(currEl)) {
+            newArray.push(currEl);
+        }
+    }
+    return newArray;
 };
 
 
