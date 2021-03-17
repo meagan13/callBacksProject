089114d44e28cf0b1387dec3fea453e847c2186c
meagan <meagan.h.smith@gmail.com>
2021-03-17 17:32:08 -0500

    Complete problem 13

--- problems/13-min-value-callback.js
+++ problems/13-min-value-callback.js
@@ -9,9 +9,40 @@ console.log(minValueCallback([64, 25, 49, 9, 100]));             // 9
 console.log(minValueCallback([64, 25, 49, 9, 100], Math.sqrt));  // 3
 
 *******************************************************************************/
+//Understand
+//  Accepts an array and optional callback
+//  Differentiate if callback and not-callback
+//  Iterate through array
+//      Hold min value
+//      Return min value
 
-let minValueCallback = function() {
+//Plan
+//  Create current minValue variable
+//  If callback === undefined, iterate through and hold minValue
+//  If callback is defined, iterate through array and hold minValue
+//  Return minValue
 
+let minValueCallback = function(array, callback) {
+    let curMinVal = -Infinity;
+
+    if(callback === undefined) {
+        for(let i = 0; i < array.length; i++) {
+            let curVal = array[i];
+
+            if(curVal < curMinVal || curMinVal === -Infinity) {
+                curMinVal = curVal;
+            }
+        }
+    } else {
+        for(let i = 0; i < array.length; i++) {
+            let curVal = array[i];
+
+            if(callback(curVal) < curMinVal || curMinVal === -Infinity) {
+                curMinVal = callback(curVal);
+            }
+        }
+    }
+    return curMinVal;
 };
 
 
