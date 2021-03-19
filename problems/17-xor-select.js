b06410e696f949d0022355cdbc218e7a90befddd
meagan <meagan.h.smith@gmail.com>
2021-03-19 17:59:25 -0500

    Completed problem 17

--- problems/17-xor-select.js
+++ problems/17-xor-select.js
@@ -29,10 +29,34 @@ console.log(
   xorSelect(["art", "academy", "app", "cat", "buttery"], longString, startsA)
 );
 // [ 'art', 'app', 'buttery' ]
+
+Write a function `xorSelect` that accepts an array and two callback as arguments.
+The function should return a new array containing elements of the original array
+that result in true when passed in one of the callbacks, but not both.
+
+Understand:
+  Accepts an array and two callbacks
+  Returns a new array
+  New array contains all elements that results in true from exactly one callback
+
+Plan:
+  Create a new array
+  Loop over the array
+  If the current element is true for cb1 or cb2 but NOT for cb1 AND cb2, push to the new array
+
 *******************************************************************************/
 
-let xorSelect = function() {
+let xorSelect = function(array, cb1, cb2) {
+  let newArray = [];
 
+  for(let i = 0; i < array.length; i++) {
+    let currentEl = array[i];
+
+    if((cb1(currentEl) || cb2(currentEl)) && !(cb1(currentEl) && cb2(currentEl))) {
+      newArray.push(currentEl);
+    }
+  }
+  return newArray;
 };
 
 
