21ab6a876971e156779bcd8889bdc84424136a5a
meagan <meagan.h.smith@gmail.com>
2021-03-19 18:13:11 -0500

    Complete problem 21

--- problems/21-at-most.js
+++ problems/21-at-most.js
@@ -20,10 +20,36 @@ console.log(atMost(['boat', 'cat', 'car'], 1, startsWithA));                    
 console.log(atMost(['boat', 'cat', 'car', 'academy'], 1, startsWithA));         // true
 console.log(atMost(['boat', 'arc', 'cat', 'car', 'academy'], 1, startsWithA));  // false
 
+Write a function `atMost` that accepts an array, a max number, and a callback as
+arguments. The function should return a boolean indicating whether or not there are
+at most (fewer than or equal to) `max` elements of the array that result in true
+when passed into the callback.
+
+Understand:
+    Accepts an array, a max number, and a callback
+    Return a boolean
+        - Is the number of true results equal to or fewer than the max number?
+Plan:
+    Create a true counter
+    Loop over the array
+        -Apply the callback
+        -If invoking the callback results in true, increase true counter
+    If the true counter is less than or equal to the max, return true
+
 *******************************************************************************/
 
-let atMost = function() {
+let atMost = function(array, max, callback) {
+    let trueCounter = 0;
 
+    for(let i = 0; i < array.length; i++) {
+        let currentEl = array[i];
+
+        if(callback(currentEl) === true) {
+            trueCounter++;
+        }
+    }
+
+    return (trueCounter <= max);
 };
 
 
