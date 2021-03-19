49759743a402ac7536f4a2fc841d59e50be8ce00
meagan <meagan.h.smith@gmail.com>
2021-03-19 17:38:42 -0500

    Completed problem 20

--- problems/20-none.js
+++ problems/20-none.js
@@ -1,9 +1,5 @@
 /*******************************************************************************
-Write a function `none` that accepts an array and a callback as arguments.
-The function should call the callback for each element of the array, passing in
-the element. The function should return true if all
-elements of the array result to false when passed into the callback. Otherwise,
-the method should return false.
+
 
 Examples:
 
@@ -27,18 +23,48 @@ let result4 = none([4, -5, 7, -1], function(n) {
 });
 console.log(result4);   // false
 
+Write a function `none` that accepts an array and a callback as arguments.
+The function should call the callback for each element of the array, passing in
+the element. The function should return true if all
+elements of the array result to false when passed into the callback. Otherwise,
+the method should return false.
+
 Understand:
     Accept an array and a callback
     Call the callback for each element of the array
     If all elements passed through the call back are false, return true
+Plan:
+    Create a false counter
+    Loop over the array
+    Invoke the callback with every element
+    If the false counter equals the length of the array, return true
+    Else, return false
 *******************************************************************************/
 
-let none = function() {
+let none = function(array, callback) {
+    let falseCounter = 0;
 
+    for(let i = 0; i < array.length; i++) {
+        let currentEl = array[i];
+
+        if(callback(currentEl) === false) {
+            falseCounter++;
+        }
+    }
+
+    return falseCounter === array.length;    //Is the number of false returns is the same as the number of elements?
 };
 
 
+let result1 = none(['ruby', 'topaz', 'opal'], function(w) {
+    return w.includes('e');
+});
+console.log(result1);   // true
 
+let result2 = none(['ruby', 'topaz', 'sapphire', 'opal'], function(w) {
+    return w.includes('e');
+});
+console.log(result2);   // false
 
 
 
