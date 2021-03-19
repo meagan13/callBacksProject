b63fff3980c20e9aaf16cb76565e7f72affe3c62
meagan <meagan.h.smith@gmail.com>
2021-03-19 18:26:06 -0500

    Complete problem 23

--- problems/23-alternating-map.js
+++ problems/23-alternating-map.js
@@ -33,11 +33,43 @@ let whisper = function (s) {
 console.log(alternatingMap(['hEy', 'EVERYone', 'whats', 'uP??'], yell, whisper));
 // [ 'HEY!', '..everyone..', 'WHATS!', '..up??..' ]
 
+Write a function `alternatingMap` that accepts an array and two callbacks as
+arguments. The function should return a new array containing the results of passing
+the original elements into the callbacks in an alternating fashion.
+
+Understand:
+    Accept an array and two callbacks
+    Return a new array
+        - add the results of invoking the callbacks, alternating between the results of cb1 and cb2
+Plan:
+    Create a new array
+    Loop over the array
+    Invoke the callbacks and push to the new array
 *******************************************************************************/
 
+const greaterCallbackValue = require("./19-greater-callback-value");
 
-let alternatingMap = function() {
 
+let alternatingMap = function(array, cb1, cb2) {
+    let newArray = [];
+
+    for(let i = 0; i < array.length; i++) {
+        let currentEl = array[i];
+
+        if(i % 2 === 0) {                       //if the index is even
+            console.log(currentEl);
+            newArray.push(cb1(currentEl));
+            console.log(newArray);    //invoke cb1 and push the result to the array
+        }
+
+        if(i % 2 !== 0) {
+            console.log(currentEl);
+            newArray.push(cb2(currentEl));      //invoke cb2 and push the result to the array
+            console.log(newArray);
+        }
+
+    }
+    return newArray;
 };
 
 
